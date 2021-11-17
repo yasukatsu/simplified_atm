16c19bc2acc89cd29a7f7c2992aa84a5263193dc
Yasukastsu.A <handbuuuu@gmail.com>
2021-11-17 22:14:50 +0900

    別のパターンでもう一つ作成(container exit)

---
+++ frontend_copy/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h2>
+      <img class="image" src="./assets/logo.png">
+      でぶとれ銀行
+    </h2>
+      <el-menu default-active="activeIndex" mode="horizontal" router>
+        <el-menu-item index="init" :route="{ name:'init' }" plain>
+          <i class="el-icon-menu"></i>
+          <span>口座開設</span>
+        </el-menu-item>
+        <el-menu-item index="receive" :route="{ name:'receive' }" plain>
+          <i class="el-icon-menu"></i>
+          <span>入金</span>
+        </el-menu-item>
+        <el-menu-item index="pay" :route="{ name:'pay' }" plain>
+          <i class="el-icon-menu"></i>
+          <span>出金</span>
+        </el-menu-item>
+      </el-menu>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      activeIndex: this.$route.name
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+h2 {
+  display: flex;
+  align-items: center;
+}
+h2 .image {
+  width: 5%;
+  height: auto;
+}
+</style>
---
+++ frontend_copy/src/components/Pay.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pay">
+    <h1>This is an pay page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pay'
+}
+</script>
